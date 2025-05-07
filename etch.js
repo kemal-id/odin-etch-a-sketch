dc3f0910e92f5551fd6b318d2026e38e34b40de8
kemal-id <54977402+kemal-id@users.noreply.github.com>
2025-05-07 12:26:28 +0700

    Work out generating square divs

--- etch.js
+++ etch.js
@@ -1,1 +1,12 @@
-alert("this works!");
+const containerDiv = document.querySelector(".container");
+const squareDiv = document.createElement("div");
+squareDiv.className = "square-div";
+
+let squareCount = 16;
+let squareTotal = 0;
+
+while (squareTotal < squareCount * squareCount) {
+  let clonedNode = squareDiv.cloneNode();
+  containerDiv.appendChild(clonedNode);
+  squareTotal++;
+}
