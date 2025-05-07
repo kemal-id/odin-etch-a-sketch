917c4b0a08cdb0ec55291456f847cdf81f4d0e14
kemal-id <54977402+kemal-id@users.noreply.github.com>
2025-05-07 13:08:04 +0700

    Work out on limiting div by square count

--- etch.js
+++ etch.js
@@ -2,7 +2,7 @@ const containerDiv = document.querySelector(".container");
 const squareDiv = document.createElement("div");
 squareDiv.className = "square-div";
 
-let squareCount = 16;
+let squareCount = 20;
 let squareTotal = 0;
 
 while (squareTotal < squareCount * squareCount) {
@@ -10,3 +10,9 @@ while (squareTotal < squareCount * squareCount) {
   containerDiv.appendChild(clonedNode);
   squareTotal++;
 }
+
+const squares = document.querySelectorAll(".square-div");
+console.log(squares);
+squares.forEach(element => {
+  element.setAttribute("style", `flex: 0 0 ${100 / squareCount}%; height: ${100 / squareCount}vh`);
+});
